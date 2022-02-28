a4bdd139a79d41b8d0f0cdbb0383d7f64025b3ad
Kaio Silveira <silveira.kaio@icloud.com>
2022-02-28 19:41:29 +0000

    ✨ add http code for UNAUTHORIZED

--- src/presentation/enumerators/http-codes.js
+++ src/presentation/enumerators/http-codes.js
@@ -2,4 +2,5 @@ export const OK = 200;
 export const CREATED = 201;
 export const NO_CONTENT = 204;
 export const BAD_REQUEST = 400;
+export const UNAUTHORIZED = 401;
 export const INTERNAL_SERVER_ERROR = 500;
